dbfcbaace48c22a56f8085e8501210dbebdf95df
indingpig <indoing@gmail.com>
2024-11-07 16:47:12 +0800

    添加px到rem的自动计算

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import postCssPxtorem from 'postcss-pxtorem';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,4 +14,19 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+	css: {
+		postcss: {
+			plugins: [
+				postCssPxtorem({
+					rootValue: 16,
+					propList: ['*'],
+					selectorBlackList: ['html'],
+					minPixelValue: 0,
+					unitPrecision: 5,
+					replace: true,
+					mediaQuery: false,
+				})
+			]
+		}
+	}
 })
