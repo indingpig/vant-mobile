d1080f2d7ab0b17ba8aa06cc8baed2b602a3f072
indingpig <indoing@gmail.com>
2024-11-11 18:23:53 +0800

    添加自定义颜色

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+			// maxWidth: {
+			// 	'139.79px': '139.79px',
+			// }
+		},
+		colors: {
+			transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+			lt: {
+				blue: 'rgb(22 190 207/var(--tw-bg-opacity));'
+			}
+		}
   },
   variants: {
     extend: {},
