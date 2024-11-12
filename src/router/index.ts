aad14e13698447a695929b38c8ce0708825c4d1c
indingpig <indoing@gmail.com>
2024-11-12 18:42:59 +0800

    代码格式化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,6 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import Flashcard from '@/views/FlashCard/Flashcard.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import Flashcard from '@/views/FlashCard/FlashCard.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -16,6 +16,6 @@ const router = createRouter({
 			component: Flashcard,
 		}
   ],
-})
+});
 
-export default router
+export default router;
