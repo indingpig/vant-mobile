bed6bd42e2cb075a8d2ff9a97e417b56bddf6a55
indingpig <indoing@gmail.com>
2024-11-14 18:19:09 +0800

    提取type

---
+++ src/types/FlashCard.d.ts
@@ -0,0 +1,35 @@
+export interface ListItem {
+	context: string;
+	url: string;
+}
+
+export interface MediaItem extends ListItem {
+	imageUrl: string;
+}
+
+export interface CardContent {
+	language: string;
+	wordAudioUrl: string;
+	sentenceAudioUrl: string;
+	word: string;
+	sentence: string;
+}
+
+export interface Card {
+	imgUrl: string;
+	content: CardContent[];
+}
+
+export interface FloderItem {
+	title: string;
+	unit: string;
+	img: string;
+	cards: Card[];
+}
+
+export interface Subject {
+	imgSub?: ListItem;
+	alist?: ListItem[];
+	h1: string;
+	floders: FloderItem[];
+}
