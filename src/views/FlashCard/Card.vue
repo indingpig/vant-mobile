22dacf2cf3e851da29f71b901a1e0e02e7635f82
indingpig <indoing@gmail.com>
2024-11-14 18:19:34 +0800

    提取组件

---
+++ src/views/FlashCard/Card.vue
@@ -0,0 +1,108 @@
+<template>
+	<div class="select-none overflow-hidden rounded-b bg-lt-blue">
+		<div class="relative w-full py-4 flex justify-center">
+			<div class="h-[456px] w-[320px] px-2">
+				<div class="h-full w-full rounded bg-white hover:cursor-pointer">
+					<div class="flex h-full w-full flex-col items-center gap-2">
+						<!-- 卡片图片与播放 -->
+						<div class="w-full flex-auto overflow-hidden">
+							<div class="flex h-full w-full flex-col items-center overflow-auto">
+								<!-- 图片 -->
+								<div class="flex flex-shrink-0 flex-grow items-center outline-none mx-10">
+									<img v-lazy="content.imgUrl" alt="">
+								</div>
+								<!-- 播放 -->
+								<div class="w-full flex-auto flex-grow max-h-[200px]">
+									<div class="flex h-full w-full flex-auto flex-col items-center justify-between px-4">
+										<div :class="[item.language ? wordClass : progressClass]" v-for="(item, index) in content.content" :key="index">
+											<!-- {{ index }} -->
+											<div class="flex flex-row items-center justify-end" v-if="index === 0">
+												<PlayAudio
+													:AudioUrl="item.wordAudioUrl"
+													:sentence="item.word"
+													:languageIcon="item.language"
+													:showIcon="true"
+													@update-progress="updateProgress"
+												/>
+												<p>. </p>
+												<PlayAudio
+													:AudioUrl="item.sentenceAudioUrl"
+													:sentence="item.sentence"
+													:languageIcon="item.language"
+													:showIcon="false"
+													@update-progress="updateProgress"
+												/>
+											</div>
+											<div class="h-0.5 w-0 bg-lt-blue" v-if="(index + 1) % 2 === 0" :style="{ width: progressWidth + '%' }"></div>
+											<div class="flex flex-row items-center justify-end" v-if="index === 2">
+												<PlayAudio
+													:AudioUrl="item.wordAudioUrl"
+													:sentence="item.word"
+													:languageIcon="item.language"
+													:showIcon="true"
+													@update-progress="updateProgress"
+												/>
+												<p>. </p>
+												<PlayAudio
+													:AudioUrl="item.sentenceAudioUrl"
+													:sentence="item.sentence"
+													:languageIcon="item.language"
+													:showIcon="false"
+													@update-progress="updateProgress"
+												/>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<!-- 按钮 -->
+						<div class="flex flex-row items-center justify-center gap-2">
+							<i class="flex items-center justify-center h-8 w-8 text-lt-blue iconfont font-sise-26 icon-xiangzuo" @click="prevCard">
+							</i>
+							<span class="inline-flex h-8 w-8 items-center justify-center text-sm text-gray-400">1</span>
+							<i class="flex items-center justify-center h-8 w-8 text-lt-blue iconfont font-sise-26 icon-xiangyou" @click="nextCard">
+							</i>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- <div>
+		</div> -->
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref, type PropType } from 'vue';
+import type { Card } from '@/types/FlashCard.d.ts';
+import PlayAudio from './PlayAudio.vue';
+defineProps({
+	content: {
+		type: Object as PropType<Card>,
+		required: true,
+	}
+});
+
+const wordClass = ref('flex w-full flex-auto flex-col items-center justify-center gap-2');
+
+const progressClass = ref('h-0.5 w-full bg-gray-200');
+
+const progressWidth = ref(0);
+
+const updateProgress = (progress: number) => {
+	progressWidth.value = progress;
+};
+
+const prevCard = () => {
+	console.log('prevCard');
+};
+const nextCard = () => {
+	console.log('nextCard');
+};
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/views/FlashCard/PlayAudio.vue
@@ -0,0 +1,69 @@
+<template>
+	<div>
+		<audio :src="AudioUrl" ref="audioRef" @play="handlePlay" @ended="handleEnded"></audio>
+		<button type="button" class="text-black audio-btn disabled:opacity-50 flex items-baseline"
+			@click="playAudio" :disabled="playDisabled">
+			<i class="iconfont mr-2 text-lt-blue"
+				v-if="showIcon"
+				:class="{
+					'icon-xibanyayu': languageIcon === 'Spanish',
+					'icon-english-filled': languageIcon === 'English'
+				}"
+			></i>
+			<p class="text-center text-current text-xl" v-html="sentence"></p>
+		</button>
+	</div>
+</template>
+
+<script setup lang="ts">
+import { ref, onUnmounted } from 'vue';
+defineProps({
+	AudioUrl: String,
+	sentence: String,
+	languageIcon: String,
+	showIcon: Boolean,
+});
+
+const emits = defineEmits(['update-progress']);
+
+const audioRef = ref<HTMLAudioElement | null>(null);
+const playDisabled = ref(false);
+let animationFrame: number | null = null;
+const progressWidth = ref(0);
+
+const playAudio = () => {
+	audioRef.value?.play();
+};
+
+const handlePlay = () => {
+	playDisabled.value = true;
+	updateProgress();
+};
+
+const updateProgress = () => {
+	if (audioRef.value) {
+		const progress = audioRef.value.currentTime / audioRef.value.duration;
+		progressWidth.value = progress * 100;
+		emits('update-progress', progressWidth.value);
+	}
+	animationFrame = requestAnimationFrame(updateProgress);
+};
+
+const handleEnded = () => {
+	playDisabled.value = false;
+	emits('update-progress', 0);
+	if (animationFrame) {
+		cancelAnimationFrame(animationFrame);
+	}
+};
+
+onUnmounted(() => {
+	if (animationFrame) {
+		cancelAnimationFrame(animationFrame);
+	}
+});
+</script>
+
+<style lang="scss" scoped>
+
+</style>
