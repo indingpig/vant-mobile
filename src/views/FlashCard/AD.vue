6edff1b739bdce0cebee5a96443eed5f49d46deb
indingpig <indoing@gmail.com>
2025-02-20 11:48:26 +0800

    获取广告

--- src/views/FlashCard/AD.vue
+++ src/views/FlashCard/AD.vue
@@ -1,23 +1,23 @@
 <template>
-	<div>
-		<img src="" alt="" @click="goToAD" />
+	<div class="relative h-24 w-full bg-lt-blue" :class="{ 'hidden': !imgUrl }">
+		<img :src="imgUrl" alt="" @click="goToAD" />
 	</div>
 </template>
 
 <script setup lang="ts">
 import { ref, onMounted } from 'vue';
-import { getADImgApi } from '@/server/api/FlashCardApi';
+import { getADListApi } from '@/server/api/FlashCardApi';
 
 const props = defineProps<{
-	api: string;
+	subjectId: string;
 }>();
 const imgUrl = ref('');
 const targetAdUrl = ref('');
 const getADImg = () => {
-	getADImgApi(props.api).then((res) => {
-		debugger;
-		imgUrl.value = res.data.imgUrl;
-		targetAdUrl.value = res.data.targetAdUrl;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	getADListApi(props.subjectId).then((res: any) => {
+		imgUrl.value = res.rows[0]?.advertiseImg;
+		targetAdUrl.value = res.rows[0]?.advertiseLink;
 	});
 };
 
